61ca806619f4efeccc273efee7dff1ef9990f30b
Vaishnavi Shrivastava <shrivastavavaishnavi77@gmail.com>
2025-08-29 23:59:01 +0530

    date topic done

---
+++ date.js
@@ -0,0 +1,61 @@
+
+
+// const d = new Date();
+// // console.log(d);
+// // console.log(d.toDateString());
+// // console.log(d.toString());
+// // console.log(d.toISOString());
+// console.log(typeof d);
+
+const { log } = require("console");
+
+// console.log(d.getDate());
+// console.log(d.getDay());
+// console.log(d.getMonth());
+// console.log(d.getFullYear());
+// console.log(d.getMilliseconds());
+// console.log(d.getMinutes());
+// console.log(d.getTime()); // time in milliseconds from 1 Jan 1970 to now
+
+
+
+
+// const now = Date.now()
+// console.log(now);// time in milliseconds from 1 Jan 1970 to now
+
+// const d = new Date("2020-02-11")//string is normal date format
+// const d1 = new Date(2020, 1, 11); // numder is 0 based
+// // year, month, date, hours, minutes, seconds, milliseconds
+// console.log(d);
+// console.log(d.toDateString());
+
+// const d = new Date();
+// d.setDate(14)
+// d.setFullYear(2025)
+// d.setMonth(10)
+// d.setHours(5)
+// d.setMinutes(30)    
+// console.log(d);
+
+
+//date calculation
+// const d1 = new Date();
+// const d2 = new Date("2027-11-25");
+
+// const date = d2-d1
+// const days = Math.floor(date/(1000*60*60*24))
+// console.log(days);
+
+// const hours = Math.floor((date/(1000*60*60))%24)
+// console.log(hours);
+
+// const minutes = Math.floor((date/(1000*60))%60)
+// console.log(minutes);
+
+// const seconds = Math.floor((date/1000)%60)
+// console.log(seconds);
+// console.log(`olympic will start in ${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`);
+
+
+
+
