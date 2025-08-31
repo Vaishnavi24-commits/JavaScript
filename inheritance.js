982198a22d730a50ca9fb030fd5bb3c504dbf554
Vaishnavi Shrivastava <shrivastavavaishnavi77@gmail.com>
2025-08-31 15:49:40 +0530

    concept of prototype chaining

---
+++ inheritance.js
@@ -0,0 +1,18 @@
+// user1 = {
+//     name:"vaishnavi",
+//     age:18
+// }
+// user2 = {
+//     amount:88,
+//     card:8997878787
+// }
+// user1.__proto__=user2 // it will set the prototype of user1 to user2
+// console.log(user1);
+// console.log(user2);
+// console.log(user1.name);
+
+// let arr = [1,2,3,4,5]
+// console.log(arr.__proto__==Array.prototype); // it will return true because arr is an instance of Array
+// console.log(arr.__proto__.__proto__==Object.prototype);// it will return true because Object is the prototype of Array
+// console.log(arr.__proto__.__proto__.__proto__==null); // it will return true because Object is the topmost prototype
+
