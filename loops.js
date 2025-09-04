53ad82b0062db390c6e3a82c40fe4484fc1c7750
Vaishnavi Shrivastava <shrivastavavaishnavi77@gmail.com>
2025-09-04 16:32:13 +0530

    completed basic loops

---
+++ loops.js
@@ -0,0 +1,103 @@
+//if-else
+//let age=8
+// if(age>=18){
+//     console.log("Eligible to vote");
+// }
+// else{
+//     console.log("Not eligible to vote");
+    
+// }
+
+//if else if else
+// let age = 14
+// if(age<18){
+//     console.log("KID");
+    
+// }
+// else if (age>45){
+//     console.log("OLD");
+    
+// }
+// else{
+//     console.log("YOUNG");
+    
+// }
+
+// switch(new Date().getDay()){
+//     case 0:{
+//         console.log("Monday");
+//         break;
+        
+//     }
+//     case 1:{
+//         console.log("Tuesday");
+//         break;
+        
+//     }
+//     case 2:{
+//         console.log("Wednesday");
+//         break;
+        
+//     }
+//     case 3:{
+//         console.log("Thrusday");
+//         break
+        
+//     }
+//     case 4:{
+//         console.log("Friday");
+//         break;
+        
+//     }
+//     case 5:{
+//         console.log("Saturday");
+//         break;
+//     }
+        
+//         default:{
+//             console.log("Not a valid Day");
+            
+ 
+//         }
+//     }
+//     // in switch statement their is a strict check like if the input is in "0" it would give default output === "0"!=0
+
+
+//for loop
+
+// for(const i=0;i<20;i++){
+//     console.log("Hello");
+    
+// }
+
+
+//sum of 10 num
+// let sum=0;
+// for(let i=0;i<=10;i++){
+//     sum+=i;
+// }
+// console.log(sum);
+
+//12345
+//12345
+//12345
+//12345
+//12345
+//12345
+// for(j=0;j<6;j++){
+// for(let i=1;i<=5;i++){
+//     console.log(i);
+    
+// }
+// }
+
+
+//while loop
+// let i=1
+// while(i<6){
+//     console.log(i);
+//     i++
+    
+// }
+
+//do-while
