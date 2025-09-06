1315d7528e56911d4d6952ee21fd6e7b48617de6
Vaishnavi Shrivastava <shrivastavavaishnavi77@gmail.com>
2025-09-06 16:43:58 +0530

    why array does not use for in loop

---
+++ forInLoopFacts.js
@@ -0,0 +1,25 @@
+//for-in loop is not taken with array
+// let a = [1,2,3,4]
+
+// // for(let key in a){
+// //     console.log(key);
+    
+// // }
+// //output-prints the indexs
+// //0
+// //1
+// //2
+// //3
+
+// //as we know array ka typeof object h so we can add a value
+// a.name="vaishnavi"
+// for(let key in a){
+//     console.log(key);
+    
+// }
+// //output-it prints indexs + name like this a index of an array can never be a variable that is why we dont use for in loop in array
+// //0
+// //1
+// //2
+// //3
+// //name
