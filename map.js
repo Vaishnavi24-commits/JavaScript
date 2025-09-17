1878052e21f7c7eb3e758b14d41415303ca9d5cb
Vaishnavi Shrivastava <shrivastavavaishnavi77@gmail.com>
2025-09-18 00:12:20 +0530

    we can modify the val of arr and obj using map function

---
+++ map.js
@@ -0,0 +1,16 @@
+// const arr= [1,2,3,4,5]
+// // const result = arr.map((num)=> {
+// //     return num*num
+// // })
+// const result = arr.map(num=> num*num)
+// console.log(result);
+// //output
+// //[ 1, 4, 9, 16, 25 ]
+
+
+//chaining of filter and map
+// arr=[1,2,3,4,5]
+// const result = arr.filter((num)=>num%2==0) . map(num=>num*num)
+// console.log(result);
+// //output
+// //[ 4, 16 ]
